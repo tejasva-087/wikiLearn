6f617e2c738fc5e67b713b11fc9ef0b4123c8400
Harsh Sahu <yesh8harsh+github@gmail.com>
2025-04-06 06:35:08 +0530

    fix

--- ultrabackend/src/app.js
+++ ultrabackend/src/app.js
@@ -17,12 +17,7 @@ const AppError = require("./utils/AppError");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-  }),
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(session(config.session));
