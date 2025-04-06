c96f4343253be589302899da39c72c0ed5d542b3
Harsh Sahu <yesh8harsh+github@gmail.com>
2025-04-06 06:05:42 +0530

    fix

--- ultrafrontend/vite.config.ts
+++ ultrafrontend/vite.config.ts
@@ -18,6 +18,6 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-    allowedHosts: ["https://wikimedia.zephyyrr.in", "*"],
+    allowedHosts: ["wikimedia.zephyyrr.in"],
   },
 });
