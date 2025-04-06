bd8368f676fa7d8aca512e89514c79f12ba709eb
Harsh Sahu <yesh8harsh+github@gmail.com>
2025-04-06 06:01:53 +0530

    fix[VITE]: Added vite config

--- ultrafrontend/vite.config.ts
+++ ultrafrontend/vite.config.ts
@@ -4,19 +4,20 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-	server: {
-		port: 5174,
-		proxy: {
-			"/api": {
-				target: "http://localhost:3001",
-				changeOrigin: true,
-			},
-		},
-	},
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 517,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+    },
+    allowedHosts: ["https://wikimedia.zephyyrr.in", "*"],
+  },
 });
